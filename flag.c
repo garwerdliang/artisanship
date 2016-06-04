55012276349c5055e751fd76953d87377079e53a
garwerdliang <battleclaw@hotmail.com>
2016-06-04 11:39:20 +0000

    Add files via upload

---
+++ flag.c
@@ -0,0 +1,17 @@
+//Garwerd Liang
+//09/03/2016
+//To print out the Daniah flag
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char * argv[]) {
+   printf ("#####  ############\n");
+   printf ("#####  ############\n");
+   printf ("#####  ############\n");
+   printf ("\n");
+   printf ("#####  ############\n");
+   printf ("#####  ############\n");
+   printf ("#####  ############\n");
+   return EXIT_SUCCESS;
+}
